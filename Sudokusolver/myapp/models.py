38c78b68a2fd7f62ba18b3d275848d38acd3c544
RajeshJinagam <rajeshjinagam28@gmail.com>
2024-12-09 14:01:45 +0530

    changes added

--- Sudokusolver/myapp/models.py
+++ Sudokusolver/myapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+import json
 
 # Create your models here.
+
+class SudokuMatrix(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    matrix = models.TextField()  # Store serialized matrix (JSON)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def set_matrix(self, matrix_data):
+        self.matrix = json.dumps(matrix_data)
+
+    def get_matrix(self):
+        return json.loads(self.matrix)
